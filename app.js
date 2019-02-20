c1505408376ee080ebdb040e924ac7d9383b5c17
toletole412 <yoonjioh90@gmail.com>
2019-02-21 02:44:52 +0900

    Test debug

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var chalk = require('chalk');
+//var debug = require('debug')('app');
 
 var app = express();
 
@@ -6,6 +8,6 @@ app.get('/', function(req, res) {
     res.send('Hello from my app');
 });
 
-app.listen(3000, function() {
-    console.log('listening on port 3000');
+app.listen(3001, function() {
+    console.log('listening on port ' + chalk.green('3001'));
 });

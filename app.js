beb9bf6152ee1aa676f64733d2ef98f078d95db8
toletole412 <yoonjioh90@gmail.com>
2019-02-21 02:51:37 +0900

    Render index.html

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var chalk = require('chalk');
 //var debug = require('debug')('app');
+//var path = require('path'); //built-in methods
 
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello from my app');
+    res.sendFile(__dirname + '/views/index.html');
+    //__dirname: current file (here, app.js)
+    //but if we use path method, we can code like this 
+    //res.sendFile(path.join(__dirname, views, index.html'));
+    //path methods ignore '/'
 });
 
 app.listen(3001, function() {

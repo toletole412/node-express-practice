4ee6fe4c44beed87934bc32d646f6068c74bc800
toletole412 <yoonjioh90@gmail.com>
2019-02-24 00:23:04 +0900

    Separate BookList and Single Book

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,7 +5,7 @@ const bookRouter = express.Router(); //encapsulate all routes
 bookRouter.route('/')
   .get((req, res) => {
       res.render(
-          'books',
+          'bookListView',
           { 
             nav: [
                 {link: '/books', title: 'Books'},
@@ -14,9 +14,18 @@ bookRouter.route('/')
         } );
   });
 
-bookRouter.route('/single')
+bookRouter.route('/:id') //for rendering single book using id
   .get((req, res) => {
-      res.send('hello single book');
+      const { id } = req.params;
+      res.render(
+        'bookView',
+        { 
+          nav: [
+              {link: '/books', title: 'Books'},
+              {link: '/authors', title: 'Authors'}],
+          title: 'Library',
+          //book: books[id]
+      } );
   });
 
 

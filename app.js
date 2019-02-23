856a3ff08d3cc749987a986b4c09094f3bb12811
toletole412 <yoonjioh90@gmail.com>
2019-02-24 00:10:59 +0900

    Add router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path'); //built-in methods
 
 var app = express();
 const port = process.env.PORT || 3000; 
+const bookRouter = express.Router(); //encapsulate all routes
 
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -13,6 +14,25 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs'); //set default engine
+
+bookRouter.route('/')
+  .get((req, res) => {
+      res.render(
+          'books',
+          { 
+            nav: [
+                {link: '/books', title: 'Books'},
+                {link: '/authors', title: 'Authors'}],
+            title: 'Books' 
+        } );
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+      res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
 
 app.get('/', function(req, res) {
     //--below is the way to send as a file
@@ -22,7 +42,15 @@ app.get('/', function(req, res) {
     //res.sendFile(path.join(__dirname, views, index.html'));
     //path methods ignore '/'
     //--below is the way using ejs
-    res.render('index', { list: ['a', 'b'], title: 'Library' });
+    res.render(
+        'index',
+        { 
+            nav: [
+                {link: '/books', title: 'Books'},
+                {link: '/authors', title: 'Authors'}],
+            title: 'Library' 
+        }
+    );
 });
 
 app.listen(port, function() {
